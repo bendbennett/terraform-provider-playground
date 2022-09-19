cb4be44b90c3b3d2dbe4a5396e8a49889ad1665c
Benjamin Bennett <synaptology@gmail.com>
2022-09-19 17:03:17 +0100

    Setup attributes and blocks for provider, resource and data source

--- internal/provider/example_data_source.go
+++ internal/provider/example_data_source.go
@@ -3,10 +3,10 @@ package provider
 import (
 	"context"
 
+	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
 	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/types"
-	"github.com/hashicorp/terraform-plugin-log/tflog"
 )
 
 var _ datasource.DataSource = (*exampleDataSource)(nil)
@@ -26,21 +26,248 @@ func (e *exampleDataSource) Metadata(ctx context.Context, req datasource.Metadat
 func (e *exampleDataSource) Schema(ctx context.Context, req datasource.SchemaRequest, resp *datasource.SchemaResponse) {
 	resp.Schema = schema.Schema{
 		Attributes: map[string]schema.Attribute{
-			"configurable_attribute": schema.StringAttribute{
-				MarkdownDescription: "Example configurable attribute",
-				Optional:            true,
-			},
 			"id": schema.StringAttribute{
-				MarkdownDescription: "Example identifier",
-				Computed:            true,
+				Computed: true,
+			},
+
+			// Simple/primitive attributes
+			"bool_attribute": schema.BoolAttribute{
+				Optional: true,
+			},
+
+			"float64_attribute": schema.Float64Attribute{
+				Optional: true,
+			},
+
+			"int64_attribute": schema.Int64Attribute{
+				Optional: true,
+			},
+
+			"list_attribute": schema.ListAttribute{
+				Optional:    true,
+				ElementType: types.StringType,
+			},
+
+			"map_attribute": schema.MapAttribute{
+				Optional:    true,
+				ElementType: types.StringType,
+			},
+
+			"number_attribute": schema.NumberAttribute{
+				Optional: true,
+			},
+
+			"object_attribute": schema.ObjectAttribute{
+				Optional: true,
+				AttributeTypes: map[string]attr.Type{
+					"bool_attribute":    types.BoolType,
+					"float64_attribute": types.Float64Type,
+					"int64_attribute":   types.Int64Type,
+					"list_attribute":    types.ListType{ElemType: types.StringType},
+					"map_attribute":     types.MapType{ElemType: types.StringType},
+					"number_attribute":  types.NumberType,
+					"set_attribute":     types.ListType{ElemType: types.StringType},
+					"string_attribute":  types.StringType,
+				},
+			},
+
+			"set_attribute": schema.SetAttribute{
+				Optional:    true,
+				ElementType: types.StringType,
+			},
+
+			"string_attribute": schema.StringAttribute{
+				Optional: true,
+			},
+
+			// Nested Attributes
+			"list_nested_attribute": schema.ListNestedAttribute{
+				Optional: true,
+				NestedObject: schema.NestedAttributeObject{
+					Attributes: map[string]schema.Attribute{
+						"int64_attribute": schema.Int64Attribute{
+							Optional: true,
+						},
+						"list_attribute": schema.ListAttribute{
+							Optional:    true,
+							ElementType: types.StringType,
+						},
+					},
+				},
+			},
+
+			"map_nested_attribute": schema.MapNestedAttribute{
+				Optional: true,
+				NestedObject: schema.NestedAttributeObject{
+					Attributes: map[string]schema.Attribute{
+						"map_attribute": schema.MapAttribute{
+							Optional:    true,
+							ElementType: types.StringType,
+						},
+						"number_attribute": schema.NumberAttribute{
+							Optional: true,
+						},
+					},
+				},
+			},
+
+			"set_nested_attribute": schema.SetNestedAttribute{
+				Optional: true,
+				NestedObject: schema.NestedAttributeObject{
+					Attributes: map[string]schema.Attribute{
+						"object_attribute": schema.ObjectAttribute{
+							Optional: true,
+							AttributeTypes: map[string]attr.Type{
+								"bool_attribute":    types.BoolType,
+								"float64_attribute": types.Float64Type,
+								"int64_attribute":   types.Int64Type,
+								"list_attribute":    types.ListType{ElemType: types.StringType},
+								"map_attribute":     types.MapType{ElemType: types.StringType},
+								"number_attribute":  types.NumberType,
+								"set_attribute":     types.ListType{ElemType: types.StringType},
+								"string_attribute":  types.StringType,
+							},
+						},
+
+						"set_attribute": schema.SetAttribute{
+							Optional:    true,
+							ElementType: types.StringType,
+						},
+						"string_attribute": schema.StringAttribute{
+							Optional: true,
+						},
+					},
+				},
+			},
+
+			"single_nested_attribute": schema.SingleNestedAttribute{
+				Optional: true,
+				Attributes: map[string]schema.Attribute{
+					"bool_attribute": schema.BoolAttribute{
+						Optional: true,
+					},
+					"float64_attribute": schema.Float64Attribute{
+						Optional: true,
+					},
+				},
+			},
+		},
+
+
+		// Nested Blocks
+		Blocks: map[string]schema.Block{
+			"list_nested_block": schema.ListNestedBlock{
+				NestedObject: schema.NestedBlockObject{
+					Attributes: map[string]schema.Attribute{
+						"bool_attribute": schema.BoolAttribute{
+							Optional: true,
+						},
+						"float64_attribute": schema.Float64Attribute{
+							Optional: true,
+						},
+
+						"int64_attribute": schema.Int64Attribute{
+							Optional: true,
+						},
+						"list_attribute": schema.ListAttribute{
+							Optional:    true,
+							ElementType: types.StringType,
+						},
+					},
+					Blocks: map[string]schema.Block{
+						"list_nested_nested_block": schema.ListNestedBlock{
+							NestedObject: schema.NestedBlockObject{
+								Attributes: map[string]schema.Attribute{
+									"bool_attribute": schema.BoolAttribute{
+										Optional: true,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+
+			"set_nested_block": schema.SetNestedBlock{
+				NestedObject: schema.NestedBlockObject{
+					Attributes: map[string]schema.Attribute{
+						"map_attribute": schema.MapAttribute{
+							Optional:    true,
+							ElementType: types.StringType,
+						},
+						"number_attribute": schema.NumberAttribute{
+							Optional: true,
+						},
+						"object_attribute": schema.ObjectAttribute{
+							Optional: true,
+							AttributeTypes: map[string]attr.Type{
+								"bool_attribute":    types.BoolType,
+								"float64_attribute": types.Float64Type,
+								"int64_attribute":   types.Int64Type,
+								"list_attribute":    types.ListType{ElemType: types.StringType},
+								"map_attribute":     types.MapType{ElemType: types.StringType},
+								"number_attribute":  types.NumberType,
+								"set_attribute":     types.ListType{ElemType: types.StringType},
+								"string_attribute":  types.StringType,
+							},
+						},
+						"set_attribute": schema.SetAttribute{
+							Optional:    true,
+							ElementType: types.StringType,
+						},
+					},
+					Blocks: map[string]schema.Block{
+						"set_nested_nested_block": schema.SetNestedBlock{
+
+							NestedObject: schema.NestedBlockObject{
+								Attributes: map[string]schema.Attribute{
+									"bool_attribute": schema.BoolAttribute{
+										Optional: true,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+
+			"single_nested_block": schema.SingleNestedBlock{
+				Attributes: map[string]schema.Attribute{
+					"bool_attribute": schema.BoolAttribute{
+						Optional: true,
+					},
+					"float64_attribute": schema.Float64Attribute{
+						Optional: true,
+					},
+				},
 			},
 		},
 	}
 }
 
 type exampleDataSourceData struct {
-	ConfigurableAttribute types.String `tfsdk:"configurable_attribute"`
-	Id                    types.String `tfsdk:"id"`
+	Id types.String `tfsdk:"id"`
+
+	BoolAttribute    types.Bool    `tfsdk:"bool_attribute"`
+	Float64Attribute types.Float64 `tfsdk:"float64_attribute"`
+	Int64Attribute   types.Int64   `tfsdk:"int64_attribute"`
+	ListAttribute    types.List    `tfsdk:"list_attribute"`
+	MapAttribute     types.Map     `tfsdk:"map_attribute"`
+	NumberAttribute  types.Number  `tfsdk:"number_attribute"`
+	ObjectAttribute  types.Object  `tfsdk:"object_attribute"`
+	SetAttribute     types.Set     `tfsdk:"set_attribute"`
+	StringAttribute  types.String  `tfsdk:"string_attribute"`
+
+	// Nested Attributes
+	ListNestedAttribute   []types.Object          `tfsdk:"list_nested_attribute"`
+	MapNestedAttribute    map[string]types.Object `tfsdk:"map_nested_attribute"`
+	SetNestedAttribute    []types.Object          `tfsdk:"set_nested_attribute"`
+	SingleNestedAttribute types.Object            `tfsdk:"single_nested_attribute"`
+
+	// Nested Blocks
+	ListNestedBlock   []types.Object `tfsdk:"list_nested_block"`
+	SetNestedBlock    []types.Object `tfsdk:"set_nested_block"`
+	SingleNestedBlock types.Object   `tfsdk:"single_nested_block"`
 }
 
 func (e *exampleDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
@@ -54,8 +281,6 @@ func (e *exampleDataSource) Read(ctx context.Context, req datasource.ReadRequest
 	}
 
 	data.Id = types.StringValue("example-id")
-
-	tflog.Trace(ctx, "read a data source")
 
 	diags = resp.State.Set(ctx, &data)
 	resp.Diagnostics.Append(diags...)
--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -3,6 +3,7 @@ package provider
 import (
 	"context"
 
+	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
 	"github.com/hashicorp/terraform-plugin-framework/provider"
 	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
@@ -20,8 +21,244 @@ func New() func() provider.Provider {
 	}
 }
 
+func (p *exampleProvider) Schema(ctx context.Context, req provider.SchemaRequest, resp *provider.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			// Simple/primitive attributes
+			"bool_attribute": schema.BoolAttribute{
+				Optional: true,
+			},
+
+			"float64_attribute": schema.Float64Attribute{
+				Optional: true,
+			},
+
+			"int64_attribute": schema.Int64Attribute{
+				Optional: true,
+			},
+
+			"list_attribute": schema.ListAttribute{
+				Optional:    true,
+				ElementType: types.StringType,
+			},
+
+			"map_attribute": schema.MapAttribute{
+				Optional:    true,
+				ElementType: types.StringType,
+			},
+
+			"number_attribute": schema.NumberAttribute{
+				Optional: true,
+			},
+
+			"object_attribute": schema.ObjectAttribute{
+				Optional: true,
+				AttributeTypes: map[string]attr.Type{
+					"bool_attribute":    types.BoolType,
+					"float64_attribute": types.Float64Type,
+					"int64_attribute":   types.Int64Type,
+					"list_attribute":    types.ListType{ElemType: types.StringType},
+					"map_attribute":     types.MapType{ElemType: types.StringType},
+					"number_attribute":  types.NumberType,
+					"set_attribute":     types.ListType{ElemType: types.StringType},
+					"string_attribute":  types.StringType,
+				},
+			},
+
+			"set_attribute": schema.SetAttribute{
+				Optional:    true,
+				ElementType: types.StringType,
+			},
+
+			"string_attribute": schema.StringAttribute{
+				Optional: true,
+			},
+
+			// Nested Attributes
+			"list_nested_attribute": schema.ListNestedAttribute{
+				Optional: true,
+				NestedObject: schema.NestedAttributeObject{
+					Attributes: map[string]schema.Attribute{
+						"int64_attribute": schema.Int64Attribute{
+							Optional: true,
+						},
+						"list_attribute": schema.ListAttribute{
+							Optional:    true,
+							ElementType: types.StringType,
+						},
+					},
+				},
+			},
+
+			"map_nested_attribute": schema.MapNestedAttribute{
+				Optional: true,
+				NestedObject: schema.NestedAttributeObject{
+					Attributes: map[string]schema.Attribute{
+						"map_attribute": schema.MapAttribute{
+							Optional:    true,
+							ElementType: types.StringType,
+						},
+						"number_attribute": schema.NumberAttribute{
+							Optional: true,
+						},
+					},
+				},
+			},
+
+			"set_nested_attribute": schema.SetNestedAttribute{
+				Optional: true,
+				NestedObject: schema.NestedAttributeObject{
+					Attributes: map[string]schema.Attribute{
+						"object_attribute": schema.ObjectAttribute{
+							Optional: true,
+							AttributeTypes: map[string]attr.Type{
+								"bool_attribute":    types.BoolType,
+								"float64_attribute": types.Float64Type,
+								"int64_attribute":   types.Int64Type,
+								"list_attribute":    types.ListType{ElemType: types.StringType},
+								"map_attribute":     types.MapType{ElemType: types.StringType},
+								"number_attribute":  types.NumberType,
+								"set_attribute":     types.ListType{ElemType: types.StringType},
+								"string_attribute":  types.StringType,
+							},
+						},
+
+						"set_attribute": schema.SetAttribute{
+							Optional:    true,
+							ElementType: types.StringType,
+						},
+						"string_attribute": schema.StringAttribute{
+							Optional: true,
+						},
+					},
+				},
+			},
+
+			"single_nested_attribute": schema.SingleNestedAttribute{
+				Optional: true,
+				Attributes: map[string]schema.Attribute{
+					"bool_attribute": schema.BoolAttribute{
+						Optional: true,
+					},
+					"float64_attribute": schema.Float64Attribute{
+						Optional: true,
+					},
+				},
+			},
+		},
+
+		// Nested Blocks
+		Blocks: map[string]schema.Block{
+			"list_nested_block": schema.ListNestedBlock{
+				NestedObject: schema.NestedBlockObject{
+					Attributes: map[string]schema.Attribute{
+						"bool_attribute": schema.BoolAttribute{
+							Optional: true,
+						},
+						"float64_attribute": schema.Float64Attribute{
+							Optional: true,
+						},
+
+						"int64_attribute": schema.Int64Attribute{
+							Optional: true,
+						},
+						"list_attribute": schema.ListAttribute{
+							Optional:    true,
+							ElementType: types.StringType,
+						},
+					},
+					Blocks: map[string]schema.Block{
+						"list_nested_nested_block": schema.ListNestedBlock{
+							NestedObject: schema.NestedBlockObject{
+								Attributes: map[string]schema.Attribute{
+									"bool_attribute": schema.BoolAttribute{
+										Optional: true,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+
+			"set_nested_block": schema.SetNestedBlock{
+				NestedObject: schema.NestedBlockObject{
+					Attributes: map[string]schema.Attribute{
+						"map_attribute": schema.MapAttribute{
+							Optional:    true,
+							ElementType: types.StringType,
+						},
+						"number_attribute": schema.NumberAttribute{
+							Optional: true,
+						},
+						"object_attribute": schema.ObjectAttribute{
+							Optional: true,
+							AttributeTypes: map[string]attr.Type{
+								"bool_attribute":    types.BoolType,
+								"float64_attribute": types.Float64Type,
+								"int64_attribute":   types.Int64Type,
+								"list_attribute":    types.ListType{ElemType: types.StringType},
+								"map_attribute":     types.MapType{ElemType: types.StringType},
+								"number_attribute":  types.NumberType,
+								"set_attribute":     types.ListType{ElemType: types.StringType},
+								"string_attribute":  types.StringType,
+							},
+						},
+						"set_attribute": schema.SetAttribute{
+							Optional:    true,
+							ElementType: types.StringType,
+						},
+					},
+					Blocks: map[string]schema.Block{
+						"set_nested_nested_block": schema.SetNestedBlock{
+
+							NestedObject: schema.NestedBlockObject{
+								Attributes: map[string]schema.Attribute{
+									"bool_attribute": schema.BoolAttribute{
+										Optional: true,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+
+			"single_nested_block": schema.SingleNestedBlock{
+				Attributes: map[string]schema.Attribute{
+					"bool_attribute": schema.BoolAttribute{
+						Optional: true,
+					},
+					"float64_attribute": schema.Float64Attribute{
+						Optional: true,
+					},
+				},
+			},
+		},
+	}
+}
+
 type providerData struct {
-	ConfigurableAttribute types.String `tfsdk:"configurable_attribute"`
+	BoolAttribute    types.Bool    `tfsdk:"bool_attribute"`
+	Float64Attribute types.Float64 `tfsdk:"float64_attribute"`
+	Int64Attribute   types.Int64   `tfsdk:"int64_attribute"`
+	ListAttribute    types.List    `tfsdk:"list_attribute"`
+	MapAttribute     types.Map     `tfsdk:"map_attribute"`
+	NumberAttribute  types.Number  `tfsdk:"number_attribute"`
+	ObjectAttribute  types.Object  `tfsdk:"object_attribute"`
+	SetAttribute     types.Set     `tfsdk:"set_attribute"`
+	StringAttribute  types.String  `tfsdk:"string_attribute"`
+
+	// Nested Attributes
+	ListNestedAttribute   []types.Object          `tfsdk:"list_nested_attribute"`
+	MapNestedAttribute    map[string]types.Object `tfsdk:"map_nested_attribute"`
+	SetNestedAttribute    []types.Object          `tfsdk:"set_nested_attribute"`
+	SingleNestedAttribute types.Object            `tfsdk:"single_nested_attribute"`
+
+	// Nested Blocks
+	ListNestedBlock   []types.Object `tfsdk:"list_nested_block"`
+	SetNestedBlock    []types.Object `tfsdk:"set_nested_block"`
+	SingleNestedBlock types.Object   `tfsdk:"single_nested_block"`
 }
 
 func (p *exampleProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
@@ -47,15 +284,5 @@ func (p *exampleProvider) DataSources(ctx context.Context) []func() datasource.D
 func (p *exampleProvider) Resources(ctx context.Context) []func() resource.Resource {
 	return []func() resource.Resource{
 		NewResource,
-	}
-}
-
-func (p *exampleProvider) Schema(ctx context.Context, req provider.SchemaRequest, resp *provider.SchemaResponse) {
-	resp.Schema = schema.Schema{
-		Attributes: map[string]schema.Attribute{
-			"configurable_attribute": schema.StringAttribute{
-				Optional: true,
-			},
-		},
 	}
 }
