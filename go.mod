0740d57cb6a836686c416d46d5dd7a2721b67bff
Benjamin Bennett <synaptology@gmail.com>
2022-09-19 17:03:17 +0100

    Setup attributes and blocks for provider, resource and data source

--- go.mod
+++ go.mod
@@ -38,6 +38,8 @@ require (
 	github.com/hashicorp/terraform-exec v0.17.3 // indirect
 	github.com/hashicorp/terraform-json v0.14.0 // indirect
 	github.com/hashicorp/terraform-registry-address v0.1.0 // indirect
+	github.com/hashicorp/terraform-plugin-log v0.7.0 // indirect
+	github.com/hashicorp/terraform-registry-address v0.0.0-20220623143253-7d51757b572c // indirect
 	github.com/hashicorp/terraform-svchost v0.0.0-20200729002733-f050f53b9734 // indirect
 	github.com/hashicorp/yamux v0.1.1 // indirect
 	github.com/huandu/xstrings v1.3.2 // indirect
--- internal/provider/example_resource.go
+++ internal/provider/example_resource.go
@@ -3,17 +3,15 @@ package provider
 import (
 	"context"
 
-	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/types"
-	"github.com/hashicorp/terraform-plugin-log/tflog"
 )
 
 var _ resource.Resource = (*exampleResource)(nil)
-var _ resource.ResourceWithImportState = (*exampleResource)(nil)
 
 type exampleResource struct {
 	provider exampleProvider
@@ -30,15 +28,222 @@ func (e *exampleResource) Metadata(_ context.Context, req resource.MetadataReque
 func (e *exampleResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
 	resp.Schema = schema.Schema{
 		Attributes: map[string]schema.Attribute{
-			"configurable_attribute": schema.StringAttribute{
-				Optional:            true,
-				MarkdownDescription: "Example configurable attribute",
-			},
 			"id": schema.StringAttribute{
-				Computed:            true,
-				MarkdownDescription: "Example identifier",
+				Computed: true,
 				PlanModifiers: []planmodifier.String{
 					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+
+			// Simple/primitive attributes
+			"bool_attribute": schema.BoolAttribute{
+				Optional: true,
+			},
+
+			"float64_attribute": schema.Float64Attribute{
+				Optional: true,
+			},
+
+			"int64_attribute": schema.Int64Attribute{
+				Optional: true,
+			},
+
+			"list_attribute": schema.ListAttribute{
+				Optional:    true,
+				ElementType: types.StringType,
+			},
+
+			"map_attribute": schema.MapAttribute{
+				Optional:    true,
+				ElementType: types.StringType,
+			},
+
+			"number_attribute": schema.NumberAttribute{
+				Optional: true,
+			},
+
+			"object_attribute": schema.ObjectAttribute{
+				Optional: true,
+				AttributeTypes: map[string]attr.Type{
+					"bool_attribute":    types.BoolType,
+					"float64_attribute": types.Float64Type,
+					"int64_attribute":   types.Int64Type,
+					"list_attribute":    types.ListType{ElemType: types.StringType},
+					"map_attribute":     types.MapType{ElemType: types.StringType},
+					"number_attribute":  types.NumberType,
+					"set_attribute":     types.ListType{ElemType: types.StringType},
+					"string_attribute":  types.StringType,
+				},
+			},
+
+			"set_attribute": schema.SetAttribute{
+				Optional:    true,
+				ElementType: types.StringType,
+			},
+
+			"string_attribute": schema.StringAttribute{
+				Optional: true,
+			},
+
+			// Nested Attributes
+			"list_nested_attribute": schema.ListNestedAttribute{
+				Optional: true,
+				NestedObject: schema.NestedAttributeObject{
+					Attributes: map[string]schema.Attribute{
+						"int64_attribute": schema.Int64Attribute{
+							Optional: true,
+						},
+						"list_attribute": schema.ListAttribute{
+							Optional:    true,
+							ElementType: types.StringType,
+						},
+					},
+				},
+			},
+
+			"map_nested_attribute": schema.MapNestedAttribute{
+				Optional: true,
+				NestedObject: schema.NestedAttributeObject{
+					Attributes: map[string]schema.Attribute{
+						"map_attribute": schema.MapAttribute{
+							Optional:    true,
+							ElementType: types.StringType,
+						},
+						"number_attribute": schema.NumberAttribute{
+							Optional: true,
+						},
+					},
+				},
+			},
+
+			"set_nested_attribute": schema.SetNestedAttribute{
+				Optional: true,
+				NestedObject: schema.NestedAttributeObject{
+					Attributes: map[string]schema.Attribute{
+						"object_attribute": schema.ObjectAttribute{
+							Optional: true,
+							AttributeTypes: map[string]attr.Type{
+								"bool_attribute":    types.BoolType,
+								"float64_attribute": types.Float64Type,
+								"int64_attribute":   types.Int64Type,
+								"list_attribute":    types.ListType{ElemType: types.StringType},
+								"map_attribute":     types.MapType{ElemType: types.StringType},
+								"number_attribute":  types.NumberType,
+								"set_attribute":     types.ListType{ElemType: types.StringType},
+								"string_attribute":  types.StringType,
+							},
+						},
+
+						"set_attribute": schema.SetAttribute{
+							Optional:    true,
+							ElementType: types.StringType,
+						},
+						"string_attribute": schema.StringAttribute{
+							Optional: true,
+						},
+					},
+				},
+			},
+
+			"single_nested_attribute": schema.SingleNestedAttribute{
+				Optional: true,
+				Attributes: map[string]schema.Attribute{
+					"bool_attribute": schema.BoolAttribute{
+						Optional: true,
+					},
+					"float64_attribute": schema.Float64Attribute{
+						Optional: true,
+					},
+				},
+			},
+		},
+
+		// Nested Blocks
+		Blocks: map[string]schema.Block{
+			"list_nested_block": schema.ListNestedBlock{
+				NestedObject: schema.NestedBlockObject{
+					Attributes: map[string]schema.Attribute{
+						"bool_attribute": schema.BoolAttribute{
+							Optional: true,
+						},
+						"float64_attribute": schema.Float64Attribute{
+							Optional: true,
+						},
+
+						"int64_attribute": schema.Int64Attribute{
+							Optional: true,
+						},
+						"list_attribute": schema.ListAttribute{
+							Optional:    true,
+							ElementType: types.StringType,
+						},
+					},
+					Blocks: map[string]schema.Block{
+						"list_nested_nested_block": schema.ListNestedBlock{
+							NestedObject: schema.NestedBlockObject{
+								Attributes: map[string]schema.Attribute{
+									"bool_attribute": schema.BoolAttribute{
+										Optional: true,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+
+			"set_nested_block": schema.SetNestedBlock{
+				NestedObject: schema.NestedBlockObject{
+					Attributes: map[string]schema.Attribute{
+						"map_attribute": schema.MapAttribute{
+							Optional:    true,
+							ElementType: types.StringType,
+						},
+						"number_attribute": schema.NumberAttribute{
+							Optional: true,
+						},
+						"object_attribute": schema.ObjectAttribute{
+							Optional: true,
+							AttributeTypes: map[string]attr.Type{
+								"bool_attribute":    types.BoolType,
+								"float64_attribute": types.Float64Type,
+								"int64_attribute":   types.Int64Type,
+								"list_attribute":    types.ListType{ElemType: types.StringType},
+								"map_attribute":     types.MapType{ElemType: types.StringType},
+								"number_attribute":  types.NumberType,
+								"set_attribute":     types.ListType{ElemType: types.StringType},
+								"string_attribute":  types.StringType,
+							},
+						},
+						"set_attribute": schema.SetAttribute{
+							Optional:    true,
+							ElementType: types.StringType,
+						},
+					},
+					Blocks: map[string]schema.Block{
+						"set_nested_nested_block": schema.SetNestedBlock{
+
+							NestedObject: schema.NestedBlockObject{
+								Attributes: map[string]schema.Attribute{
+									"bool_attribute": schema.BoolAttribute{
+										Optional: true,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+
+			"single_nested_block": schema.SingleNestedBlock{
+				Attributes: map[string]schema.Attribute{
+					"bool_attribute": schema.BoolAttribute{
+						Optional: true,
+					},
+					"float64_attribute": schema.Float64Attribute{
+						Optional: true,
+					},
+>>>>>>> b4e47a7 (Setup attributes and blocks for provider, resource and data source)
 				},
 			},
 		},
@@ -46,10 +251,32 @@ func (e *exampleResource) Schema(ctx context.Context, req resource.SchemaRequest
 }
 
 type exampleResourceData struct {
-	ConfigurableAttribute types.String `tfsdk:"configurable_attribute"`
-	Id                    types.String `tfsdk:"id"`
+	Id types.String `tfsdk:"id"`
+
+	// Simple/primitive attributes
+	BoolAttribute    types.Bool    `tfsdk:"bool_attribute"`
+	Float64Attribute types.Float64 `tfsdk:"float64_attribute"`
+	Int64Attribute   types.Int64   `tfsdk:"int64_attribute"`
+	ListAttribute    types.List    `tfsdk:"list_attribute"`
+	MapAttribute     types.Map     `tfsdk:"map_attribute"`
+	NumberAttribute  types.Number  `tfsdk:"number_attribute"`
+	ObjectAttribute  types.Object  `tfsdk:"object_attribute"`
+	SetAttribute     types.Set     `tfsdk:"set_attribute"`
+	StringAttribute  types.String  `tfsdk:"string_attribute"`
+
+	// Nested Attributes
+	ListNestedAttribute   []types.Object          `tfsdk:"list_nested_attribute"`
+	MapNestedAttribute    map[string]types.Object `tfsdk:"map_nested_attribute"`
+	SetNestedAttribute    []types.Object          `tfsdk:"set_nested_attribute"`
+	SingleNestedAttribute types.Object            `tfsdk:"single_nested_attribute"`
+
+	// Nested Blocks
+	ListNestedBlock   []types.Object `tfsdk:"list_nested_block"`
+	SetNestedBlock    []types.Object `tfsdk:"set_nested_block"`
+	SingleNestedBlock types.Object   `tfsdk:"single_nested_block"`
 }
 
+// Create is unmarshalling the config onto exampleResourceData and persisting to the state.
 func (e *exampleResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
 	var data exampleResourceData
 
@@ -62,26 +289,17 @@ func (e *exampleResource) Create(ctx context.Context, req resource.CreateRequest
 
 	data.Id = types.StringValue("example-id")
 
-	tflog.Trace(ctx, "created a resource")
-
 	diags = resp.State.Set(ctx, &data)
 	resp.Diagnostics.Append(diags...)
 }
 
+// Read is returning the contents of the state for this resource and the State field within resource.ReadResponse
+// is pre-populated so no action is required in this function.
 func (e *exampleResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
-	var data exampleResourceData
-
-	diags := req.State.Get(ctx, &data)
-	resp.Diagnostics.Append(diags...)
-
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	diags = resp.State.Set(ctx, &data)
-	resp.Diagnostics.Append(diags...)
 }
 
+// Update overwrites the state with the plan. This is required in order that optional attributes in the config
+// are updated in place.
 func (e *exampleResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
 	var data exampleResourceData
 
@@ -96,17 +314,7 @@ func (e *exampleResource) Update(ctx context.Context, req resource.UpdateRequest
 	resp.Diagnostics.Append(diags...)
 }
 
+// Delete is automatically handled by the Framework which sets an empty state. This function does not need to
+// be populated unless additional handling needs to be implemented over and above setting an empty state.
 func (e *exampleResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
-	var data exampleResourceData
-
-	diags := req.State.Get(ctx, &data)
-	resp.Diagnostics.Append(diags...)
-
-	if resp.Diagnostics.HasError() {
-		return
-	}
-}
-
-func (e *exampleResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
-	resource.ImportStatePassthroughID(ctx, path.Root("id"), req, resp)
 }
